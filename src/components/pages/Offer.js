76cbc04e5cff364fb8f02009b7f1215d6b7e4d71
Zack Calaily <Zack.Calaily@rci.rogers.ca>
2019-06-18 11:46:21 -0400

    Added pricing table for offeR

--- src/components/pages/Offer.js
+++ src/components/pages/Offer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import numeral from 'numeral';
 import { createClient } from 'contentful';
 import { SPACE_ID, ENVIRONMENT_ID, ACCESS_TOKEN } from '../../constants';
 import showdown from 'showdown';
@@ -31,6 +32,17 @@ class Offer extends React.Component {
         this.setState({ showOrHide: !this.state.showOrHide });
     }
 
+    getColumnsForProduct(product) {
+        const columns = [];
+        const scope = this.state.entry.fields.offer.fields.pricing.filter(price => price.fields.product === product);
+
+        scope.forEach(s => {
+            columns.push(<td>${numeral(Math.max(s.fields.basePrice - s.fields.discount, 0)).format('0.00')}</td>);
+        })
+
+        return columns;
+    }      
+
     render() {
         if (this.state.entry === null) {
             return null;
@@ -51,6 +63,30 @@ class Offer extends React.Component {
                         <h1>{this.state.entry.fields.title}</h1>
                         <p>{this.state.entry.fields.description}</p>
                         
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td>Starter</td>
+                                    <td>Select</td>
+                                    <td>Popular</td>
+                                    <td>Premier</td>
+                                </tr>
+                                <tr>
+                                    <td>Ignite 150u</td>
+                                    {this.getColumnsForProduct('Ignite 150u')}
+                                </tr>
+                                <tr>
+                                    <td>Ignite 500u</td>
+                                    {this.getColumnsForProduct('Ignite 500u')}
+                                </tr>
+                                <tr>
+                                    <td>Ignite Gigabit</td>
+                                    {this.getColumnsForProduct('Ignite Gigabit')}
+                                </tr>
+                            </tbody>
+                        </table>
+
                         <a href="#" onClick={this.onToggleShowOrHideHandler}>(+) SEE FULL DETAILS</a>
                         <div className={this.state.showOrHide === true ? 'collapse show' : 'collapse hide'}>
                             <div className="fees">{this.state.entry.fields.offer.fields.fees.map((fee, index) => <div key={index}>{fee.fields.description} of ${fee.fields.amount} {fee.fields.hasOwnProperty('taxesApplicable') ? 'plus taxes ' : null}applies</div>)}</div>
