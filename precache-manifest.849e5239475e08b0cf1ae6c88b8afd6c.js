b13322b89504cf94e27d57a2af69e648f60b72ac
Zack Calaily <Zack.Calaily@rci.rogers.ca>
2019-06-18 11:21:01 -0400

    Updates

---
+++ precache-manifest.849e5239475e08b0cf1ae6c88b8afd6c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58112354c563af4afde878a5d41543d7",
+    "url": "/sales-assist-preview/index.html"
+  },
+  {
+    "revision": "0e47b260ef8d9cf36e28",
+    "url": "/sales-assist-preview/static/css/main.e8283144.chunk.css"
+  },
+  {
+    "revision": "2de2bff30670c53402b2",
+    "url": "/sales-assist-preview/static/js/2.29d85216.chunk.js"
+  },
+  {
+    "revision": "0e47b260ef8d9cf36e28",
+    "url": "/sales-assist-preview/static/js/main.647b5e41.chunk.js"
+  },
+  {
+    "revision": "bf425944f42ef0e706cb",
+    "url": "/sales-assist-preview/static/js/runtime~main.447a0daa.js"
+  },
+  {
+    "revision": "cf93e03434fed38a1ef58b160519dda9",
+    "url": "/sales-assist-preview/static/media/AvenirNextLTPro-Bold.cf93e034.woff"
+  },
+  {
+    "revision": "a99af28e5dbe5bb805d07cce6ea81b8e",
+    "url": "/sales-assist-preview/static/media/AvenirNextLTPro-Demi.a99af28e.woff"
+  },
+  {
+    "revision": "75c299bf61ee68da44e1b32346621551",
+    "url": "/sales-assist-preview/static/media/AvenirNextLTPro-Light.75c299bf.otf"
+  },
+  {
+    "revision": "7c2ce0bd479821707e53c46c195c1a2b",
+    "url": "/sales-assist-preview/static/media/AvenirNextLTPro-Light.7c2ce0bd.woff"
+  },
+  {
+    "revision": "cfaf720a9bc7b1201c62b709e461a25b",
+    "url": "/sales-assist-preview/static/media/AvenirNextLTPro-Light.cfaf720a.eot"
+  },
+  {
+    "revision": "4ecbb7c5bf6c5145305168cbb37989d7",
+    "url": "/sales-assist-preview/static/media/AvenirNextLTPro-Regular.4ecbb7c5.woff"
+  },
+  {
+    "revision": "7973eb7dd485ada87c004719ebf90739",
+    "url": "/sales-assist-preview/static/media/AvenirNextLTPro-Regular.7973eb7d.eot"
+  },
+  {
+    "revision": "9e3d36a9da7d06674b0a2d6a2fba19c8",
+    "url": "/sales-assist-preview/static/media/AvenirNextLTPro-Regular.9e3d36a9.otf"
+  },
+  {
+    "revision": "dab63a1541d9d9914c40d1387a45aac8",
+    "url": "/sales-assist-preview/static/media/logo.dab63a15.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sales-assist-preview/precache-manifest.849e5239475e08b0cf1ae6c88b8afd6c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sales-assist-preview/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
